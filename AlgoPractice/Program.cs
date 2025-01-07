13bd377f0233fdb3deed6ad5a167ba5e12b139af
shalant <doug.rosenberg@gmail.com>
2025-01-07 15:54:53 -0600

    working on algo #138

--- AlgoPractice/Program.cs
+++ AlgoPractice/Program.cs
@@ -11,107 +11,194 @@ namespace AlgoPractice
         static void Main(string[] args)
         {
             //Console.WriteLine(test(new int[5] ));
-            Console.WriteLine(test(new[] { 1 }));
-            Console.WriteLine(test(new[] { 1, 2, 9 }));
-            Console.WriteLine(test(new[] { 1, 2, 9, 3, 3 }));
-            Console.WriteLine(test(new[] { 1, 2, 3, 4, 5, 6, 7 }));
-            Console.WriteLine(test(new[] { 1, 2, 2, 3, 7, 8, 9, 10, 6, 5, 4 }));
+            //Console.WriteLine("Does this array contain adjacent 3's or 5's? ");
+            Console.WriteLine("Problem #138: ");
 
-            //Console.WriteLine(stringTest("abcab"));
+            //Console.WriteLine(test(new[] { 1, 2, 3, 4, 6, 8 }));
+            //Console.WriteLine(test(new[] { 15, 2, 3, 4, 15, 11 }));
+            //Console.WriteLine(test(new[] { 3, 3, 15, 15, 5, 5 }));
+            //Console.WriteLine(test(new[] { 1, 5, 15, 7, 8, 15 }));
+
+            //Console.WriteLine(stringTest(new string[] { "a", "b", "bb", "c", "ccc" }, 3));
+            //for (int i = 0; i < 3; i++) 
+            //{
+            //    Console.Write(stringTest(new string[] { "a", "b", "bb", "c", "ccc" }, 3).ToString());
+            //}
             //Console.WriteLine(stringTest("python"));
             //Console.WriteLine(stringTest("aacda"));
             //Console.WriteLine(stringTest("jython"));
             //Console.WriteLine(stringTest("xyzsder"));
-            //int[] item1 = new[] { 1 };
-            //int[] item2 = new[] { 40, 50, 60 };
+            //int[] item = new[] { 1, 5, 7, 9, 10, 17 };
+            ////int[] item2 = new[] { 40, 50, 60 };
             //int[] result = test(item1);
 
-            //Console.WriteLine("Original array 1: ");
-            //foreach (var i in item1)
+            //foreach (var i in item)
             //{
             //    Console.Write(i.ToString() + " ");
             //}
+            //Console.WriteLine("Sum sans 17: ");
+            //Console.WriteLine(test(new[] { 3, 5, 1, 3, 7 }));
+            //Console.WriteLine(test(new[] { 1, 2, 3, 4 }));
+            //Console.WriteLine(test(new[] { 3, 3, 5, 5, 5, 5 }));
+            //Console.WriteLine(test(new[] { 2, 4, 5, 6 }));
+            //Console.WriteLine(test(new[] { 1, 5, 9, 10, 17, 5 }));
+            //int[] result = test(new[] { 1, 2, 5, 3, 5, 4, 6, 9, 11 });
+            string[] item = stringTest(new[] { "a", "aaa", "b", "bbb", "c", "ccc" }, 3);
 
-            //Console.WriteLine("New array: ");
-            //foreach (var i in result)
-            //{
-            //    Console.Write(i.ToString() + " ");
-            //}
-            //Console.ReadLine();
+            Console.WriteLine("New array: ");
+            foreach (var i in item)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.ReadLine();
         }
 
         //public static int test(int x, int y)
         //public static bool test(int x, int y)
         public static int test(int[] x)
         {
-            int first = 0;
-            int middle = 0;
-            int last = 0;
-            int max = first;
-
-            if (x.Length > 0)
+            for (int i = 0; i < x.Length / 2; i++)
             {
-                first = x[0];
-                middle = x[x.Length / 2];
-                last = x[x.Length - 1];
+                //firstSum += x[i];
+            }
+            for (int j = x.Length / 2; j < x.Length; j++)
+            {
+                //secondSum += x[j];
+            }
+            var firstHalf = Average(x, 0, x.Length /2);
+            var secondHalf = Average(x, x.Length / 2, x.Length);
 
-                if (middle > max)
-                {
-                    max = middle;
-                }
+            
+            return 2;
+        }
 
-                if (last > max)
-                {
-                    max = last;
-                }
+        private static int Average(int[] num, int start, int end)
+        {
+            var sum = 0; //variable to store sum of elements within specified range
+
+            //loop to sum elementes within the specified range
+            for (int i = start; i < end; i++)
+            {
+                sum += num[i];
             }
 
-            return max;
-            //int[] result = new int[2];
-
-            //if (x.Length >= 1)
-            //{
-            //    result[0] = x[0];
-            //    result[1] = x[x.Length - 1];
-            //}
-            //return result;
-            //int[] result = new int[2];
-            //result[0] = x[2];
-            //result[1] = y[2];
-            //return result;
-            //int[] y = new[] { 10, 20, -30, -40, 50 };
-            ////return new int[] { a1[1], a1[2], a1[3], a1[0] };
-            //int[] result = new int[5];
-            //int j = 0;
-            //for (int i = 4; i >= 0; i--) {
-            //    result[j] = y[i];
-            //    j++;
-            //}
-            //return result;
-            //return new int[] { x[1], x[2], x[3], x[0] };
-
-            //int counter = 0;
-            //if (x == 13 || x == 17) counter += x;
+            return sum / (num.Length / 2);
 
         }
 
+        //return firstHalf > secondHalf ? firstHalf : secondHalf;
+        //return (firstSum / (x.Length / 2) > secondSum / x.Length / 2) ? firstSum / x.Length / 2 : secondSum / x.Length / 2;
+        //int firstSum = 0;
+        //int secondSum = 0;
 
-        public static string stringTest(string s1)
+        //    for (int i = 0; i<x.Length / 2; i++)
+        //    {
+        //        firstSum += x[i];
+        //    }
+        //    for (int i = x.Length / 2; i<x.Length; i++)
+        //    {
+        //        secondSum += x[i];
+        //    }
+        //int[] result = new int[x.Length];
+        //int counter = 0;
+        //for (int i = 0; i < x.Length; i++)
+        //{
+        //    if (x[i] == 0)
+        //    {
+        //        result[counter] = x[i];
+        //        counter++;
+        //    }
+        //}
+        //for (int j = 0; j < x.Length - counter; j++)
+        //{
+        //    if (x[j] != 0)
+        //    {
+        //        result[j + counter] = x[j];
+        //    }
+        //}
+        //return result;
+
+
+        //while (i >= 0 && x[i] != 5)
+        //{
+        //    i--; 
+        //}
+        //i++;
+
+        //size = x.Length - i;
+        //result = new int[size];
+
+        //for (int j = 0; j< size; j++)
+        //{
+        //        result[j] = x[j+i];
+        //}
+
+        //return result;
+
+
+        //for (int i = 0; i < x.Length; i++)
+        //{
+        //    if (x[i] == 5)
+        //    {
+        //        if ((i > 0 && x[i - 1] == 5) || (i < arr_len - 1 && x[i + 1] == 5))
+        //        {
+        //            flag = true;
+        //        }
+        //        else if (i == arr_len - 1)
+        //        {
+        //            flag = false;
+        //        }
+        //        else
+        //            return false;
+        //    }
+        //}
+
+        //return flag;
+
+
+        //return max;
+        //int[] result = new int[2];
+
+        //if (x.Length >= 1)
+        //{
+        //    result[0] = x[0];
+        //    result[1] = x[x.Length - 1];
+        //}
+        //return result;
+        //int[] result = new int[2];
+        //result[0] = x[2];
+        //result[1] = y[2];
+        //return result;
+        //int[] y = new[] { 10, 20, -30, -40, 50 };
+        ////return new int[] { a1[1], a1[2], a1[3], a1[0] };
+        //int[] result = new int[5];
+        //int j = 0;
+        //for (int i = 4; i >= 0; i--) {
+        //    result[j] = y[i];
+        //    j++;
+        //}
+        //return result;
+        //return new int[] { x[1], x[2], x[3], x[0] };
+
+        //int counter = 0;
+        //if (x == 13 || x == 17) counter += x;
+
+
+
+
+        public static string[] stringTest(string[] s1, int num)
         {
-            if (s1.Length > 1)
-            {
-                if (s1.Substring(1,1) == "a")
-                {
-                    s1 = s1.Remove(1,1);
-                }
+            string[] result = new string[num];
 
-                if (s1.Substring(0, 1) == "a")
+            for (int i = 0; i < num; i++)
+            {
+                if (s1[i].Length == num)
                 {
-                    s1 = s1.Remove(0, 1);
+                    result[i] = s1[i];
                 }
             }
 
-            return s1;
+            return result;
 
 
             //if(s1.Length < 3)
--- AlgoPractice/obj/Debug/net8.0/AlgoPractice.AssemblyInfo.cs
+++ AlgoPractice/obj/Debug/net8.0/AlgoPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgoPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097e297c0c85ae69e053a4cd70335a573b212845")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87372044d4f7097183592282064627a56ea49695")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgoPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgoPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
